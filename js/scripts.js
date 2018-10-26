f198fa92eb7512285fef2215c14588794672bc3f
Madeline <Dawndylion@Dawns-iMac.local>
2018-10-25 19:42:47 -0700

    Codekitted
    Or at least I tried. Need to go back to college lol

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// First, load jQuery (required)
+// jQuery 2.2.2 loaded via `index.html`
+
+// Second, load Semantic UI JS (required)
+// @codekit-prepend "semantic.js";
+
+// Third, Add to Homescreen (optional)
+// @codekit-prepend "add-to-homescreen.js";
+
+// Third, load any plugins (optional)
+// @codekit-prepend "plugins.js";
+
+// My Scripts
