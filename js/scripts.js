80d534ff07a25d8d11e86e121c5dc01795d3568e
Madeline <Dawndylion@Dawns-iMac.local>
2018-10-27 16:10:08 -0700

    Hiding Nav configure pt 1
    Can't figure it out, can never figure js out...

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
 
-console.log('End my suffering');
+// console.log('Hello, Doomlord');
 
+var prev = 0;
+var $window = $(window);
+var nav = $('.nav');
+
+$window.on('scroll', function(){
+  var scrollTop = $window.scrollTop();
+  nav.toggleClass('hidden', scrollTop > prev);
+  prev = scrollTop;
+});
