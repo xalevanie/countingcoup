0f46dc44dba9e8f9707f36078203108a2c72504f
Madeline <Dawndylion@Dawns-iMac.local>
2018-10-27 03:01:50 -0700

    Working?
    It might be working. Getting things set  up...

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,5 @@
-// First, load jQuery (required)
-// jQuery 2.2.2 loaded via `index.html`
-
-// Second, load Semantic UI JS (required)
+// @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
 
-// Third, Add to Homescreen (optional)
-// @codekit-prepend "add-to-homescreen.js";
+console.log('End my suffering');
 
-// Third, load any plugins (optional)
-// @codekit-prepend "plugins.js";
-
-// My Scripts
