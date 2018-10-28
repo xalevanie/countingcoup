3d3cc110be9bf4f226ad9f3786dda4bc88482cd3
Madeline <Dawndylion@Dawns-iMac.local>
2018-10-27 17:23:00 -0700

    Trying to get ANY js to work...
    WTF?

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,41 @@
-// @codekit-prepend "jquery.js";
-// @codekit-prepend "semantic.js";
+// Scripts
+//console.log('Heya');
 
-// console.log('Hello, Doomlord');
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
 
-var prev = 0;
-var $window = $(window);
-var nav = $('.nav');
-
-$window.on('scroll', function(){
-  var scrollTop = $window.scrollTop();
-  nav.toggleClass('hidden', scrollTop > prev);
-  prev = scrollTop;
+$(window).scroll(function (event) {
+    didScroll = true;
 });
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 50);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
