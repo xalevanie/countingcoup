5af4dacf975d99899b7410bdec6dbb0e1f7dcc2e
Madeline <maddybrine91@gmail.com>
2018-11-07 22:02:59 -0800

    Messing with it
    Getting nothing done.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,41 @@
 // Scripts
 console.log('Heya');
 
+
+$(document).ready(function(e) {
+  
+  
+  var lastScrollTop, incr = 0;
+  var menuSpace = 20; startsOn = 80;
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+    
+    if (st > startsOn){
+      if (st > lastScrollTop){
+        incr++;
+        if (incr >= menuSpace){
+          $('nav').addClass('hide');
+          incr = menuSpace;
+        }
+      } else {
+        incr -= 1;
+        if (incr <= 5){
+          $('nav').removeClass('hide');
+          incr = 0;
+        }
+      }
+    } else {
+      $('nav').removeClass('hide');
+    }
+    lastScrollTop = st;
+  });
+  
+  
+});
+
+
+
+
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
