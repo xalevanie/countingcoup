1f42784fe8cb398bf4bf937879a5a27227a4b207
Madeline <maddybrine91@gmail.com>
2018-11-14 11:22:47 -0800

    Added Mike

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
 // Scripts
 console.log('Heya');
 
-
-$(window).scroll(function() {
-var theta = $(window).scrollTop() / 50 % Math.PI;
-$('#craycray').css({ transform: 'rotate(' + theta + 'rad)' });
-});
